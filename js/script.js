0e7de968f732fe1072c0c81ec363f1d168eb7ebb
Tukhvatulin Almaz <mewlalabla@gmail.com>
2022-08-15 15:34:33 +0300

    created
    created motion-design course page
    created error page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
 	$('.header__burger').click(function(){
-		console.log('test')
+		//console.log('test')
 		$('.header__menu').toggleClass('active');
 		$('.header__burger').toggleClass('active');
 	});
-});
+	$('.question__item').each(function(){
+		$(this).click(function(){
+			$(this).toggleClass('active');
+		})
+	})
+	$('.navigation__item:last').click(function(e){
+		e.preventDefault();
+	})
+	$('.header__link').click(function(e){
+		if ($(this).hasClass('active')){
+			e.preventDefault();
+		}
+	})
+	$('a').click(function(e){
+		if ($(this).hasClass('.navigation__item:last')){
+			
+		}
+		else if (!($(this).attr('href'))){
+			e.preventDefault();
+			console.log('new page')
+			window.open('error.html' , '_self');
+		}
+	})
+})
